e7e130c9b6adfc2edde25d070c0bd4097ac511ed
Jose Eder Marcos Lara <skrap.marcos@gmail.com>
2018-10-23 12:52:49 -0500

    Stable version

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name init01App.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the init01App
+ */
+angular.module('init01App')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name init01App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the init01App
+ */
+angular.module('init01App')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
